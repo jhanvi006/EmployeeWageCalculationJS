99bb9450217a71777d3cc8c8232ff48787a7f01f
jhanvi006 <jhanvikankhara06@gmail.com>
2022-06-09 03:02:42 +0530

    Added: daily wage storage in map added along with total wage calculation

--- EmployeeWageCalculation.js
+++ EmployeeWageCalculation.js
@@ -9,6 +9,7 @@ const MAX_HRS_IN_MONTH = 160;
 let totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage = 0, day = 0;
 let mapDayWithWage;
 let empDailyWageArray = [];
+let empDailyWageMap = new Map();
 function workingHrs(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -32,16 +33,25 @@ function mapDayWithDailyWage(dailyWage){
     day++;
     return day+"=>"+dailyWage;
 }
+function totalWages(totalWage, dailyWage){
+    return totalWage + dailyWage;
+}
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10)%3;
     let empHrs = workingHrs(empCheck);
     totalEmpHrs += empHrs;
     empDailyWageArray.push(calculateDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays, calculateDailyWage(empHrs));
 }
 empDailyWageArray.forEach(calculateTotalWage);
 mapDayWithWage = empDailyWageArray.map(mapDayWithDailyWage);
+let totalWageUsingMap = Array.from(empDailyWageMap.values()).reduce(totalWages, 0);
 console.log("Daily wage: \n"+mapDayWithWage);
+console.log("Daily wage using map: \n"+JSON.stringify([...empDailyWageMap.entries()]));
+// for(let [day, dailyWage] of empDailyWageMap)
+//     console.log(day+": "+dailyWage);
+console.log("Employee total wage using map: "+totalWageUsingMap);
 console.log("Total working days: "+totalWorkingDays+"\nTotal working hrs: "+
             totalEmpHrs+"\nTotal wage: "+totalEmpWage);
 let empWithFullTime = mapDayWithWage.filter(day => day.includes("160"));
@@ -55,4 +65,4 @@ totalDaysWorked = empDailyWageArray.reduce((totalDaysWork, dailyWage) => {
         totalDaysWork++;
     return totalDaysWork;
 });
-console.log("No. of days employee worked: "+ totalDaysWorked);
+console.log("No. of days employee worked: "+ totalDaysWorked); 
