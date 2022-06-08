78264c08331201347b6bd6d622e13cfcab46bb03
jhanvi006 <jhanvikankhara06@gmail.com>
2022-06-08 12:11:21 +0530

    Added: monthly wage calculation added.

--- EmployeeWageCalculation.js
+++ EmployeeWageCalculation.js
@@ -3,6 +3,7 @@ const IS_FULL_Time = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PR_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function workingHrs(empCheck){
     switch(empCheck){
@@ -18,7 +19,9 @@ function workingHrs(empCheck){
     return empHrs;
 }
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10)%3;
-empHrs = workingHrs(empCheck);
+for(let day = 0; day<=NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10)%3;
+    empHrs += workingHrs(empCheck);
+}
 let empWage = empHrs * WAGE_PR_HOUR;
-console.log("Employee wage: "+empWage);
+console.log("Employee's Total wage: "+empWage+" and Total working hrs: "+empHrs);
